077e837c4a639da447b9fc21e588185309e463b0
Shigekazu Takei <takei.shg@gmail.com>
2012-09-16 09:57:16 +0900

    first commit. now it implements primitive csv parser

---
+++ MyParser.hs
@@ -0,0 +1,36 @@
+module MyParser where
+
+import           Text.ParserCombinators.Parsec
+
+csvFile :: Parser [[String]]
+csvFile = do
+    result <- many line
+    eof
+    return result
+
+line :: Parser [String]
+line = do
+    result <- cells
+    eol
+    return result
+
+cells :: Parser [String]
+cells = do
+    first <- cellContent
+    next <- remainingCells
+    return (first : next)
+
+remainingCells :: Parser [String]
+remainingCells =
+    (char ',' >> cells) <|> (return [])
+
+cellContent :: Parser String
+cellContent =
+    many (noneOf ",\n")
+
+eol :: Parser Char
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
